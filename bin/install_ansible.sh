aae507338d81e9fc7f847fe68c2194643b353f37
kamimoo <arcobaleno7+git@gmail.com>
2022-07-15 15:45:33 +0900

    Use Python 3

--- bin/install_ansible.sh
+++ bin/install_ansible.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-which pip >/dev/null 2>&1
+which pip3 >/dev/null 2>&1
 if [ $? -ne 0 ];
 then
-	echo "pip is not installed"
-	echo "install pip"
-	sudo easy_install pip
+	echo "pip3 is not installed"
+	echo "install pip3"
+	sudo easy_install pip3
 fi
 
 which ansible > /dev/null 2>&1
@@ -12,6 +12,6 @@ if [ $? -ne 0 ];
 then
 	echo "ansible is not installed"
 	echo "install ansible"
-	sudo pip install ansible
+	sudo pip3 install ansible
 fi
 
