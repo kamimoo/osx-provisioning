e475c66b51de2cfb508a12a0bc233d7244c7f7dc
kamimoo <arcobaleno7+git@gmail.com>
2022-07-18 11:16:39 +0900

    Run ansible-galaxy

--- bin/install_ansible.sh
+++ bin/install_ansible.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
+
 if ! command -v ansible &> /dev/null
 then
 	echo "ansible is not installed"
@@ -7,4 +9,6 @@ then
 	sudo pip3 install --upgrade pip
 	pip3 install ansible
 fi
+
+ansible-galaxy install -r requirements.yml
 
