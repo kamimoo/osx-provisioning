5b0cf68169fc1e0f24d69cefea54b4712c3e5b8c
kamimoo <arcobaleno7+git@gmail.com>
2022-07-18 11:02:32 +0900

    Upgrade pip

--- bin/install_ansible.sh
+++ bin/install_ansible.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-which pip3 >/dev/null 2>&1
-if [ $? -ne 0 ];
-then
-	echo "pip3 is not installed"
-	echo "install pip3"
-	sudo easy_install pip3
-fi
 
-which ansible > /dev/null 2>&1
-if [ $? -ne 0 ];
+if ! command -v ansible &> /dev/null
 then
 	echo "ansible is not installed"
 	echo "install ansible"
-	sudo pip3 install ansible
+	sudo pip3 install --upgrade pip
+	pip3 install ansible
 fi
 
